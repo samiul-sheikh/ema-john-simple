efca15afe177b1c482f05e941b144c7813bbe224
samiul-sheikh <sheikh.samiul1971@gmail.com>
2021-02-27 10:59:52 +0600

    Component layout, use flexbox, setup area container

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,18 +1,24 @@
 import React, { useState } from 'react';
 import fakeData from '../../fakeData';
+import './Shop.css';
 
 const Shop = () => {
     const first10 = fakeData.slice(0, 10);
     const [products, setProducts] = useState(first10);
 
     return (
-        <div>
-            <h1>This is Shop</h1>
-            <ul>
-                {
-                    products.map(product => <li>{product.name}</li>)
-                }
-            </ul>
+        <div className="shop-container">
+            <div className="product-container">
+                <ul>
+                    {
+                        products.map(product => <li>{product.name}</li>)
+                    }
+                </ul>
+            </div>
+            <div className="cart-container">
+                <h3>This is Cart</h3>
+            </div>
+
         </div>
     );
 };
