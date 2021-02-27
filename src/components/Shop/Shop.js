44428d802e54481616579aef52d10c83a53fa03d
samiul-sheikh <sheikh.samiul1971@gmail.com>
2021-02-28 03:29:16 +0600

    Cart state setup and update cart state on add to cart button click

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -7,8 +7,12 @@ const Shop = () => {
     const first10 = fakeData.slice(0, 10);
     const [products, setProducts] = useState(first10);
 
+    const [cart, setCart] = useState([])
+
     const handleAddProduct = (product) => {
         console.log('product added', product);
+        const newCart = [...cart, product];
+        setCart(newCart);
     }
 
     return (
@@ -23,6 +27,7 @@ const Shop = () => {
             </div>
             <div className="cart-container">
                 <h3>This is Cart</h3>
+                <h5>Order summary: {cart.length}</h5>
             </div>
 
         </div>
