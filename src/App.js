096d74a9e0aff750bf44e9787c9ddd4e120ff4d9
samiul-sheikh <sheikh.samiul1971@gmail.com>
2021-03-06 02:13:02 +0600

    Setup Router, Router path, exact path, not found path

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import './App.css';
 import Header from './components/Header/Header';
 import Shop from './components/Shop/Shop';
+import Review from './components/Review/Review';
+import Inventory from './components/Inventory/Inventory';
+import NotFound from './components/NotFound/NotFound';
 import {
     BrowserRouter as Router,
     Switch,
@@ -14,7 +17,25 @@ function App() {
     return (
         <div>
             <Header></Header>
-            <Shop></Shop>
+            <Router>
+                <Switch>
+                    <Route path="/shop">
+                        <Shop></Shop>
+                    </Route>
+                    <Route path="/review">
+                        <Review></Review>
+                    </Route>
+                    <Route path="/inventory">
+                        <Inventory></Inventory>
+                    </Route>
+                    <Route exact path="/">
+                        <Shop></Shop>
+                    </Route>
+                    <Route path="*">
+                        <NotFound></NotFound>
+                    </Route>
+                </Switch>
+            </Router>
         </div>
     );
 }
