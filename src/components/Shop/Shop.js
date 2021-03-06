2ce4ed8d7cd6660cef724275e729cfd0f8531733
samiul-sheikh <sheikh.samiul1971@gmail.com>
2021-03-06 19:35:22 +0600

    Save state on Localstore, store key and count, virtual DOM

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import fakeData from '../../fakeData';
+import { addToDatabaseCart } from '../../utilities/databaseManager';
 import Cart from '../Cart/Cart';
 import Product from '../Product/Product';
 import './Shop.css';
@@ -13,6 +14,9 @@ const Shop = () => {
     const handleAddProduct = (product) => {
         const newCart = [...cart, product];
         setCart(newCart);
+        const sameProduct = newCart.filter(pd => pd.key === product.key);
+        const count = sameProduct.length;
+        addToDatabaseCart(product.key, count);
     }
 
     return (
@@ -20,6 +24,7 @@ const Shop = () => {
             <div className="product-container">
                 {
                     products.map(pd => <Product
+                        key={pd.key}
                         product={pd}
                         handleAddProduct={handleAddProduct}
                         showAddToCart={true}>
