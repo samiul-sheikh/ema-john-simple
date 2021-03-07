dd43ae10a13fe53c84b8574ac905de28d2341413
samiul-sheikh <sheikh.samiul1971@gmail.com>
2021-03-07 12:55:05 +0600

    Review Component, Review Items, CSS in JavaScript style

--- src/components/Review/Review.js
+++ src/components/Review/Review.js
@@ -1,9 +1,10 @@
 import React, { useEffect, useState } from 'react';
 import { getDatabaseCart } from '../../utilities/databaseManager';
 import fakeData from '../../fakeData';
+import ReviewItem from '../ReviewItem/ReviewItem';
 
 const Review = () => {
-    const { cart, setCart } = useState([]);
+    const [cart, setCart] = useState([]);
 
     useEffect(() => {
         //cart
@@ -15,12 +16,17 @@ const Review = () => {
             product.quantity = savedCart[key];
             return product;
         });
-        console.log(cartProducts);
-    })
+        setCart(cartProducts);
+    }, [])
 
     return (
         <div>
-            <h1>This is Review</h1>
+            <h1>Cart Items:{cart.length}</h1>
+            {
+                cart.map(pd => <ReviewItem 
+                    key={pd.key}
+                    product={pd}></ReviewItem>)
+            }
         </div>
     );
 };
---
+++ src/components/ReviewItem/ReviewItem.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const ReviewItem = (props) => {
+    // console.log(props);
+    const {name, quantity} = props.product;
+
+    const reviewItemStyle={
+        borderBottom: '1px solid lightgray',
+        marginBottom: '5px',
+        paddingBottom: '5px',
+        marginLeft: '200px'
+    }
+    
+    return (
+        <div style={reviewItemStyle} className="review-item">
+            <h4 className="product-name">{name}</h4>
+            <p>Quantity: {quantity}</p>
+            <br/>
+            <button className="main-button">Review</button>
+        </div>
+    );
+};
+
+export default ReviewItem;
